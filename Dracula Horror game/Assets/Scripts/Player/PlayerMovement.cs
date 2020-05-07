2de5157f40eb56be92cf0e0857553441e2e75548
JordanG <26260@ma-web.nl>
2020-05-07 11:03:14 +0200

    Crouch added and cant stand up if there is a roof is too close above you

--- Dracula Horror game/Assets/Scripts/Player/PlayerMovement.cs
+++ Dracula Horror game/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 6;
     [SerializeField] private float runSpeed = 12;
     [SerializeField] private float walkSpeed = 6;
+    [SerializeField] private float crouchSpeed = 3;
     [SerializeField] private float gravity = -9.81f;
     [SerializeField] private float groundDistance = 0.4f;
     [SerializeField] private float jumpHeight = 3;
@@ -16,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
 
     private Transform groundCheck;
+    private Transform roofCheck;
     [SerializeField] private LayerMask groundMask;
 
     [SerializeField] private bool grounded = false;
@@ -25,6 +27,7 @@ public class PlayerMovement : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         groundCheck = transform.GetChild(1);
+        roofCheck = transform.GetChild(4);
     }
 
     // Update is called once per frame
@@ -41,14 +44,37 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * movement.x + transform.forward * movement.y;
 
-        if (Input.GetButton("Sprint") && grounded)
+        if (Input.GetButton("Sprint") && grounded && controller.height > 1)
         {
+            if (controller.height < 2)
+            {
+                ScaleHeight(true);
+            }            
             speed = runSpeed;
         }
         else
         {
-            speed = walkSpeed;
-        }       
+            if (Input.GetButton("Crouch") && grounded)
+            {
+                if (controller.height > 1)
+                {
+                    ScaleHeight(false);
+                }
+                speed = crouchSpeed;
+            }
+            else
+            {
+                if (controller.height < 2)
+                {
+                    ScaleHeight(true);
+                }
+
+                if (controller.height > 1)
+                {
+                    speed = walkSpeed;
+                }                
+            }
+        }   
 
         controller.Move(move * speed * Time.deltaTime);
 
@@ -60,5 +86,24 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void ScaleHeight(bool up)
+    {
+        if (up)
+        {
+            if (!Physics.CheckSphere(roofCheck.position, groundDistance, groundMask))
+            {
+                controller.height = 2;
+                transform.GetChild(3).localScale = new Vector3(1, 1f, 1);
+                Camera.main.transform.localPosition = new Vector3(0, 0.8f, 0);
+            }            
+        }
+        else
+        {
+            controller.height = 1;
+            transform.GetChild(3).localScale = new Vector3(1, 0.5f, 1);
+            Camera.main.transform.localPosition = new Vector3(0, 0.25f, 0);
+        }
     }
 }
