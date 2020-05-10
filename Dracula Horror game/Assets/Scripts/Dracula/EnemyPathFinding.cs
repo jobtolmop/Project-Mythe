98bbe69393359628b8920d6bd0b747ac5e546cfe
JordanG <26260@ma-web.nl>
2020-05-10 22:17:38 +0200

    NavMeshAgent added

---
+++ Dracula Horror game/Assets/Scripts/Dracula/EnemyPathFinding.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPathFinding : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    private Transform player;
+    private Vector3 targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPos = player.position;
+
+        agent.SetDestination(targetPos);
+    }
+}
--- Dracula Horror game/Assets/Scripts/Player/PlayerMovement.cs
+++ Dracula Horror game/Assets/Scripts/Player/PlayerMovement.cs
@@ -11,99 +11,108 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float walkSpeed = 6;
     [SerializeField] private float crouchSpeed = 3;
     [SerializeField] private float gravity = -9.81f;
-    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private float roofDistance = 0.4f;
     [SerializeField] private float jumpHeight = 3;
 
     private Vector3 velocity = Vector3.zero;
-
-    private Transform groundCheck;
     private Transform roofCheck;
     [SerializeField] private LayerMask groundMask;
 
-    [SerializeField] private bool grounded = false;
+    [SerializeField] private bool crouching = false;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        groundCheck = transform.GetChild(1);
         roofCheck = transform.GetChild(4);
     }
 
     // Update is called once per frame
     void Update()
     {
-        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        if (grounded && velocity.y < 0)
+        Vector3 move = transform.right * movement.x + transform.forward * movement.y;        
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (controller.isGrounded)
+        {
+            if (!crouching)
+            {
+                if (Input.GetButton("Sprint"))
+                {
+                    if (speed < runSpeed)
+                    {
+                        speed += 7 * Time.deltaTime;
+                    }
+                }
+                else
+                {
+                    if (speed > walkSpeed)
+                    {
+                        speed -= 10 * Time.deltaTime;
+                    }
+                    else
+                    {
+                        speed = walkSpeed;
+                    }
+                }
+            }
+
+            if (Input.GetButtonDown("Crouch"))
+            {
+                Crouch(!crouching);
+
+                if (crouching)
+                {
+                    speed = crouchSpeed;
+                }
+            }
+        }
+
+        if (controller.isGrounded && velocity.y < 0)
         {
             velocity.y = -2;
         }
 
-        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
-        Vector3 move = transform.right * movement.x + transform.forward * movement.y;
-
-        if (Input.GetButton("Sprint") && grounded && controller.height > 1)
-        {
-            if (controller.height < 2)
-            {
-                ScaleHeight(true);
-            }            
-            speed = runSpeed;
-        }
-        else
-        {
-            if (Input.GetButton("Crouch") && grounded)
-            {
-                if (controller.height > 1)
-                {
-                    ScaleHeight(false);
-                }
-                speed = crouchSpeed;
-            }
-            else
-            {
-                if (controller.height < 2)
-                {
-                    ScaleHeight(true);
-                }
-
-                if (controller.height > 1)
-                {
-                    speed = walkSpeed;
-                }                
-            }
-        }   
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && grounded)
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
 
         velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
     }
 
-    private void ScaleHeight(bool up)
+    private void Crouch(bool down)
     {
-        if (up)
+        if (!down)
         {
-            if (!Physics.CheckSphere(roofCheck.position, groundDistance, groundMask))
+            if (!Physics.CheckSphere(roofCheck.position, roofDistance, groundMask))
             {
+                crouching = false;
                 controller.height = 2;
                 transform.GetChild(3).localScale = new Vector3(1, 1f, 1);
-                Camera.main.transform.localPosition = new Vector3(0, 0.8f, 0);
+                Camera.main.transform.localPosition = new Vector3(0, 0.8f, Camera.main.transform.localPosition.z);
             }            
         }
         else
         {
+            crouching = true;
             controller.height = 1;
             transform.GetChild(3).localScale = new Vector3(1, 0.5f, 1);
-            Camera.main.transform.localPosition = new Vector3(0, 0.25f, 0);
+            Camera.main.transform.localPosition = new Vector3(0, 0.25f, Camera.main.transform.localPosition.z);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (roofCheck != null)
+        {
+            Gizmos.DrawWireSphere(roofCheck.position, roofDistance);
+        }        
     }
 }
