27ed046aff7402c51a2783c8e37988370e35f6ab
JobTol <26710@ma-web.nl>
2020-05-27 09:06:09 +0200

    Revert "Merge branch 'Job' of https://github.com/jobtolmop/Project-Mythe into Job"
    This reverts commit 656f7e4fd2144962a529221767400fc2c3dadd41, reversing
    changes made to 983ce46bd458064c6e9ec465ab52f4ef4bb2a5eb.

--- Dracula Horror game/Assets/Scripts/Player/PlayerMovement.cs
+++ Dracula Horror game/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,146 +6,59 @@ public class PlayerMovement : MonoBehaviour
 {
     private CharacterController controller;
 
-    private Vector2 movement;
     [SerializeField] private float speed = 6;
     [SerializeField] private float runSpeed = 12;
     [SerializeField] private float walkSpeed = 6;
-    [SerializeField] private float crouchSpeed = 3;
     [SerializeField] private float gravity = -9.81f;
-    [SerializeField] private float roofDistance = 0.4f;
+    [SerializeField] private float groundDistance = 0.4f;
     [SerializeField] private float jumpHeight = 3;
 
     private Vector3 velocity = Vector3.zero;
 
-    [SerializeField] private Transform roofCheck;
-    [SerializeField] private Transform physicsObject;
+    private Transform groundCheck;
     [SerializeField] private LayerMask groundMask;
-    [SerializeField] private bool crouching = false;
-    [SerializeField] private bool jumping = false;
 
-    public bool Crouching { get { return crouching; } }
-    public bool Jumping { get { return jumping; } }
-    public Vector2 Movement { get { return movement; } }
-    public bool CantMove { get; set; } = false;
-
-    private Transform cam;
-
-    private PlayerSoundMaker soundMaker;
+    [SerializeField] private bool grounded = false;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        cam = Camera.main.transform;
-        //soundMaker = GetComponent<PlayerSoundMaker>();
+        groundCheck = transform.GetChild(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!CantMove)
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (grounded && velocity.y < 0)
         {
-            movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            velocity.y = -2;
+        }
 
-            Vector3 move = transform.right * movement.x + transform.forward * movement.y;
-            if (move.magnitude > 1)
-            {
-                move.Normalize();
-            }
-            //Move is called before controls like jump and sprint check because isGrounded must be called after move()
-            controller.Move(move * speed * Time.deltaTime);
-        }        
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        controller.Move(velocity * Time.deltaTime);
+        Vector3 move = transform.right * movement.x + transform.forward * movement.y;
 
-        if (controller.isGrounded)
+        if (Input.GetButton("Sprint") && grounded)
         {
-            if (jumping)
-            {
-                jumping = false;
-                //soundMaker.PlayCoroutineJumpLand();
-            }
-
-            if (Input.GetButton("Sprint"))
-            {
-                if (crouching)
-                {
-                    Crouch(false);
-                }
-
-                if (speed < runSpeed) 
-                {
-                    speed += 7 * Time.deltaTime;
-                }
-            }
-            else
-            {
-                if (!crouching)
-                {
-                    if (speed > walkSpeed)
-                    {
-                        speed -= 10 * Time.deltaTime;
-                    }
-                    else
-                    {
-                        speed = walkSpeed;
-                    }
-                }               
-            }
-
-            if (Input.GetButtonDown("Crouch"))
-            {
-                Crouch(!crouching);
-
-                if (crouching)
-                {
-                    speed = crouchSpeed;
-                }
-            }
-
-            if (velocity.y < 0)
-            {
-                velocity.y = -2;
-            }
-
-            if (Input.GetButtonDown("Jump"))
-            {                
-                jumping = true;
-                //soundMaker.PlayCoroutineJumpLand();
-                velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-            }
-        }        
-
-        velocity.y += gravity * Time.deltaTime;
-    }
-
-    private void Crouch(bool down)
-    {
-        if (!down)
-        {
-            if (!Physics.CheckSphere(roofCheck.position, roofDistance, groundMask))
-            {
-                crouching = false;
-                controller.height = 2;
-                physicsObject.localScale = Vector3.one;
-                cam.localPosition = new Vector3(0, 0.8f, cam.localPosition.z);
-            }            
+            speed = runSpeed;
         }
         else
         {
-            crouching = true;
-            controller.height = 1;
-            physicsObject.localScale = new Vector3(1, 0.5f, 1);
-            cam.localPosition = new Vector3(0, 0.25f, cam.localPosition.z);
-        }
-    }
+            speed = walkSpeed;
+        }       
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        if (roofCheck != null)
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && grounded)
         {
-            Gizmos.DrawWireSphere(roofCheck.position, roofDistance);
-        }        
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
--- Dracula Horror game/Assets/Scripts/Player/PlayerPickup.cs
+++ Dracula Horror game/Assets/Scripts/Player/PlayerPickup.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerPickup : MonoBehaviour
 {
-<<<<<<< HEAD
     float throwForce = 40000f;
     Vector3 objectPos;
     float distance;
@@ -25,61 +24,23 @@ public class PlayerPickup : MonoBehaviour
         //check if isHolding
         if (isHolding == true)
         {
-=======
-    private float throwForce = 40000f;
-    private Vector3 objectPos;
-    private float distance;
-
-    private bool thrown = false;
-    [SerializeField] private GameObject item;
-    [SerializeField] private GameObject tempParent;
-    [SerializeField] private bool isHolding = false;
-    private bool canHold = true;
-
-    private void Start()
-    {
-        tempParent = Camera.main.gameObject;
-    }
-
-    private void Update()
-    {
-        //check if isHolding
-        if (isHolding)
-        {
-            item.gameObject.layer = 11;
-            item.transform.GetChild(0).gameObject.layer = 11;
->>>>>>> master
             item.GetComponent<Rigidbody>().velocity = Vector3.zero;
             item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
             item.transform.SetParent(tempParent.transform);
 
             if (Input.GetMouseButtonDown(1))
             {
-<<<<<<< HEAD
                 item.GetComponent<Rigidbody>().AddForce(tempParent.transform.forward * throwForce);
-=======
-                item.gameObject.layer = 11;
-                item.transform.GetChild(0).gameObject.layer = 11;
-                thrown = true;
-                canHold = false;
-                StartCoroutine("HoldDelay");
->>>>>>> master
                 isHolding = false;
             }
         }
         else
         {
-<<<<<<< HEAD
-=======
-            item.gameObject.layer = 12;
-            item.transform.GetChild(0).gameObject.layer = 12;
->>>>>>> master
             objectPos = item.transform.position;
             item.transform.SetParent(null);
             item.GetComponent<Rigidbody>().useGravity = true;
             item.transform.position = objectPos;
         }
-<<<<<<< HEAD
     }
 
     private void OnMouseDown()
@@ -98,37 +59,3 @@ public class PlayerPickup : MonoBehaviour
         isHolding = false;
     }
 }
-=======
-
-        if (Input.GetMouseButtonUp(0) && isHolding)
-        {
-            isHolding = false;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (thrown)
-        {
-            item.GetComponent<Rigidbody>().AddForce(tempParent.transform.forward * throwForce);
-            thrown = false;
-        }        
-    }
-
-    private IEnumerator HoldDelay()
-    {
-        yield return new WaitForSeconds(1);
-        canHold = true;
-    }
-
-    public void Pickedup()
-    {
-        if (canHold)
-        {
-            GetComponent<SoundEffectProp>().PersonThatPushed = tempParent.transform.parent.gameObject;
-            isHolding = true;
-            item.GetComponent<Rigidbody>().useGravity = false;
-        }        
-    }
-}
->>>>>>> master
